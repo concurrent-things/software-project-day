0b8419ce6f7514d47deceeeff9805e18cfb1aa44
Kevin Hartman <kevin@hart.mn>
2013-01-09 18:01:23 -0500

    Abstract method for setting up a day's events by concrete implementors

--- src/Employee.java
+++ src/Employee.java
@@ -1,13 +1,15 @@
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 
-public abstract class Employee {
+public abstract class Employee extends Thread {
 
 	private ConcurrentLinkedQueue<Runnable> activeTaskQueue = new ConcurrentLinkedQueue<Runnable>();
 	
 	public void enqueueTask(Runnable newActiveTask) {
 		activeTaskQueue.add(newActiveTask);
 	}
+	
+	public abstract void registerDaysEvents();
 
 
 }
