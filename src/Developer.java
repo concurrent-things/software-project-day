d332715741a266b5398a6a985c1f89b4564cc84e
sxb5828 <sxb5828@BABBINGTON.se.rit.edu>
2013-01-09 18:01:39 -0500

    Added some skeleton methods to the class.

--- src/Developer.java
+++ src/Developer.java
@@ -1,4 +1,29 @@
 
-public class Developer {
-
+public class Developer extends Employee{
+	
+	private int teamNumber;
+	private int teamMemberNumber;
+	
+	
+	public Developer(int teamNumber, int teamMemberNumber) { 
+		
+		this.teamNumber = teamNumber; 
+		this.teamMemberNumber = teamMemberNumber; 
+	}
+	
+	public void askQuestion() { 	
+		
+	}
+	
+	public void goToLunch() { 
+		 	
+	}
+	
+	public void provideStatusUpdated() { 
+		
+	}
+	
+	public void run() { 
+		
+	}
 }
