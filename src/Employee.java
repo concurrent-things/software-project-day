23d46b1591f0de12524cd5d3e9fdbee214b225a3
Kevin Hartman <kevin@hart.mn>
2013-01-09 17:27:53 -0500

    Added a queue for Employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,13 @@
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 
 public abstract class Employee {
 
-	//contains lunch runnable
+	private ConcurrentLinkedQueue<Runnable> activeTaskQueue = new ConcurrentLinkedQueue<Runnable>();
+	
+	public void enqueueTask(Runnable newActiveTask) {
+		activeTaskQueue.add(newActiveTask);
+	}
+
 
 }
