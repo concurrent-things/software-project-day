f03fbb1f7268fdc0210a5a0b7abf3152ae4ee480
Shun Mok Bhark <sxb5828@rit.edu>
2013-01-14 17:30:24 -0500

    Added new empty runnables for a developer.

--- src/Developer.java
+++ src/Developer.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -31,39 +32,70 @@ public class Developer extends Employee{
 		startDevTime();
 		calculateDevLunch();
 		calculateDevEndTime();
-		
 	}
 	
+	//Runnables 
+	final protected Runnable endOfDayLeave = new Runnable() {
+
+		public void run() {
+			
+		} 
+	};
+	
+	
+	final protected Runnable goToLunch = new Runnable() { 
+		
+		public void run() { 
+			
+		}
+	};
+	
+	/** 
+	 * Method that sets the simulated start time of the developer. 
+	 */
 	private void startDevTime()  { 
 		
 		devDayStartTime = System.nanoTime();
 	}
 	
+	/** 
+	 * Method that randomly generates a simulated lunch time period for the developer 
+	 */
 	private void calculateDevLunch() { 
 		
 		//Randomly generating time for lunch 
 		Random randomGen = new Random(); 
 		int randomLunchTime = randomGen.nextInt(600) + 300;
+		devLunchTime = TimeUnit.NANOSECONDS.convert(randomLunchTime, TimeUnit.MILLISECONDS);
 		
 	}
 	
+	/**
+	 * Method that calculates the simulated end time of the developer. 
+	 */
 	private void calculateDevEndTime() { 
 		
+		long modifiedStartTime = devDayStartTime + devLunchTime;
+		devDayEndTime = modifiedStartTime + (TimeUnit.NANOSECONDS.convert(4800, TimeUnit.MILLISECONDS));
 		
 	}
 	
+	/**
+	 * Getter method to return team number of developer
+	 * @return
+	 */
 	public int getDevTeamNumber() { 
 		
 		return teamNumber; 
 	}
 	
+	/**
+	 * Getter method to return the number of the developer in the team
+	 * @return
+	 */
 	public int getDevTeamMemberNumber() { 
 		
 		return teamMemberNumber; 
-	}
-	
-	public void goToLunch() { 
-		 	
 	}
 	
 	public void provideStatusUpdated() { 
